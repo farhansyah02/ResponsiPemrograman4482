3f36914583434f83ca989f24ea1138377753613b
farhansyah02 <101778751+farhansyah02@users.noreply.github.com>
2022-04-21 09:49:56 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ResponsiPemrograman4482
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("190302123", "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "Jono", 2000000);
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1. "); karyawan1.tampilkanData();
+            Console.Write("2. "); karyawan2.tampilkanData();
+            Console.WriteLine();
+
+            karyawan1.naikGaji(); // menaikkan gaji
+            karyawan2.naikGaji(); // menaikkan gaji
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.tampilkanData();
+            Console.Write("2 "); karyawan2.tampilkanData();
+
+            Console.WriteLine();
+            Console.Write("tekan sembarang key untuk menyudahi....");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ResponsiPemrograman4482
+{
+    public class Karyawan
+    {
+        private string nik;
+        private string nama;
+        private int gajiBulanan;
+
+        public string Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gajiBulanan; }
+            set { gajiBulanan = value; }
+        }
+
+        public Karyawan(string nik, string nama, int gajiBulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+            this.gajiBulanan = gajiBulanan < 0 ? 0 : gajiBulanan;
+        }
+
+        public void tampilkanData()
+        {
+            Console.WriteLine("{0} {1}      {2}", this.nik, this.nama, this.gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            int kenaikan;
+
+            kenaikan = gajiBulanan * 110;
+            this.gajiBulanan = kenaikan / 100;
+
+            Console.WriteLine("asyik naik gaji 10%");
+        }
+    }
+}
